09a615ebb5c9baa569b898538864e435c0172db8
alexp1203 <alexpinheiro1203@gmail.com>
2023-04-20 23:50:59 -0300

    corrigido javascript

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const passwordtwo = document.getElementById('passwordtwo')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordtwo = document.getElementById('password-two');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault() //remove o comportamento padrão do navegaor
+    e.preventDefault()
 
     checkInputs()
 })
@@ -27,17 +27,21 @@ function checkInputs() {
 
     if(emailValue === '') {
         errorValidation(email, 'Preencha esse campo')
+    } else if (!isEmail(emailValue)){
+        errorValidation(email, 'E-mail invalido')
     } else {
         successValidation(email)
     }
 
     if(passwordValue === '') {
         errorValidation(password, 'Preencha esse campo')
-    } else if(passwordValue.length < 8) {
+    } else if (passwordValue.length < 8) {
         errorValidation(password, 'A senha deve ter 8 caracteres')
+    } else{
+        successValidation(password)
     }
 
-    if(passwordtwoValeu === ''){
+    if(passwordtwoValue === ''){
         errorValidation(passwordtwo, 'Preencha esse campo')
     } else if (passwordValue !== passwordtwoValue) {
         errorValidation(passwordtwo, 'Senha não é a mesma')
@@ -47,7 +51,7 @@ function checkInputs() {
 }
 
 function errorValidation(input, message) {
-    const formControl = input.parrentElement; //parentElement retorna uma referencia direta do pai
+    const formControl = input.parentElement; //parentElement retorna uma referencia direta do pai
     const small = formControl.querySelector('small')
 
     small.innerText = message
@@ -56,13 +60,12 @@ function errorValidation(input, message) {
 }
 
 function successValidation(input) {
-    const formControl = input.parrentElement;
+    const formControl = input.parentElement;
 
     formControl.className = 'form-control success'
 }
 
-/*
+
 function isEmail(email) {
     return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
 }
-*/
